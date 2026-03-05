269290d5a7f2c9ab42a5cdbdcc6c25f7c92c75c8
Nguyễn Tấn Tài <demohandsome@gmail.com>
2026-03-05 12:07:33 +0700

    Edit main.js

--- StudyCard/Practice3/src/main.js
+++ StudyCard/Practice3/src/main.js
@@ -1,3 +1,9 @@
 $("#btnHello").on("click", function () {
     alert("Hello from jQuery!");
 });
+
+const btnRed = document.getElementById("btnRed");
+
+btnRed.addEventListener("click", function () {
+    document.body.style.fontSize = '10px';
+})
