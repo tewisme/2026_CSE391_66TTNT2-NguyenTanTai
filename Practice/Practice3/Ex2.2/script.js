6358701484ee42c628eb6673dd88c33064d5d34a
Nguyễn Tấn Tài <demohandsome@gmail.com>
2026-03-15 21:10:45 +0700

    DONE Ex2.2!!!!!

--- Practice/Practice3/Ex2.2/script.js
+++ Practice/Practice3/Ex2.2/script.js
@@ -5,15 +5,17 @@ const addressInput = document.getElementById('address');
 const notesInput = document.getElementById('notes');
 const paymentInput = document.getElementById('payment');
 
+const btnBuy = document.getElementById('btn-buy');
+
 const prices = {
   "book": 1,
   "bible": 2,
   "rd": 0
 }
 
-const btnBuy = document.getElementById('btn-buy');
 
-dateInput.addEventListener('blur', function(event){
+
+dateInput.addEventListener('input', function(event){
   event.preventDefault();
   let date1 = new Date(dateInput.value);
   let date2 = new Date();
@@ -23,28 +25,39 @@ dateInput.addEventListener('blur', function(event){
   {
     warn.textContent = 'Ngày giao phải trong vòng 30 ngày';
   }
+  else
+  {
+    warn.textContent = "";
+  }
 });
 
-quantityInput.addEventListener('blur', function(event){
+quantityInput.addEventListener('input', calTotal)
+function calTotal(event){
   event.preventDefault();
   let quantity = Number(quantityInput.value);
   let name = productInput.value;
   let warn = document.getElementById('warn-quantity');
+  console.log(quantity);
   if (quantity < 1 || quantity > 99)
   {
     warn.textContent = "Vui lòng nhập số lượng hợp lệ!";
   }
   else if ( name != 'default' )
   {
-    warn.textContent = "Hóa đơn của bạn là: " + quantity*prices[name] + "vnd";
+    warn.textContent = "Hóa đơn của bạn là: " + (quantity*prices[name]).toLocaleString("vi-VN") + "vnd";
     warn.style.color = 'black';
   }
-});
+  else
+  {
+    warn.textContent = "";
+  }
+};
 
 productInput.addEventListener('blur', function(event){
   event.preventDefault();
   let product = productInput.value;
   let warn = document.getElementById('warn-items');
+  calTotal(event);
   if ( product == 'default' )
   {
     warn.textContent = "Vui lòng chọn sản phẩm";
