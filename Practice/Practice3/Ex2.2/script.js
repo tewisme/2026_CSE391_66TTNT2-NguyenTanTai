9fc8b0484aeaa60d0d040f635a66c507190dd4b4
Nguyễn Tấn Tài <demohandsome@gmail.com>
2026-03-15 20:14:43 +0700

    Ex2.2 loading..

---
+++ Practice/Practice3/Ex2.2/script.js
@@ -0,0 +1,9 @@
+const productInput = document.getElementById('products');
+const quantityInput = document.getElementById('quantity');
+const 
+
+const btnBuy = document.getElementById('btn-buy');
+
+btnBuy.addEventListener('click', function(event){
+  event.preventDefault();
+});
